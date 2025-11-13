0c02b622d2cc74cd21ee02cecf970f926b1967ba
Erica Mavilia <ericamavilia@MacBook-Air-di-Erica.local>
2025-11-13 11:02:39 +0100

    Aggiunta Libretto

--- src/main/java/it/universita/model/Libretto.java
+++ src/main/java/it/universita/model/Libretto.java
@@ -1,4 +1,19 @@
 package it.universita.model;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Libretto {
+    private ArrayList<Esame> esami = new ArrayList<>();
+
+
+
+    public Libretto() {}
+
+    public void aggiungiEsameAlLibretto(Esame esame) {
+        esami.add(esame);
+    }
+
+    public ArrayList<Esame> getEsami() {
+        return esami;
+    }
 }
