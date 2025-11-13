a44c64ba6a0948759ce9354d444a11c2bdfa1dab
Francesco Calascione <kalashnik0v_19@MacBook-Air-di-Francesco.local>
2025-11-13 16:21:33 +0100

    aggiunte classi Corso e Materia

--- src/main/java/it/universita/model/Corso.java
+++ src/main/java/it/universita/model/Corso.java
@@ -1,4 +1,71 @@
 package it.universita.model;
 
 public class Corso {
+    private long id;
+    private String nome;
+    private String codice;
+    private int cfu;
+    private long idProfessore;
+
+public Corso() {}
+public Corso (long id, String nome, String codice, int cfu, long idProfessore) {
+    this.id = id;
+    this.nome = nome;
+    this.codice = codice;
+    this.cfu = cfu;
+    this.idProfessore = idProfessore;
+}
+
+public long getId() {
+    return id;
+}
+
+public void setId(long id) {
+        this.id = id;
+}
+
+public String getNome() {
+    return nome;
+}
+
+public void setNome(String nome) {
+    this.nome = nome;
+}
+
+public String getCodice() {
+    return codice;
+}
+
+public void setCodice(String codice) {
+    this.codice = codice;
+}
+
+
+public int getCfu() {
+    return cfu;
+}
+
+public void setCfu(int cfu) {
+    this.cfu = cfu;
+}
+
+public long getIdProfessore() {
+    return idProfessore;
+}
+
+public void setIdProfessore(long idProfessore) {
+    this.idProfessore = idProfessore;
+}
+
+@Override
+public String toString() {
+    return "Corso{" +
+            "id=" + id +
+            ", codice='" + codice + '\'' +
+            ", nome='" + nome + '\'' +
+            ", cfu=" + cfu +
+            ", idProfessore=" + idProfessore +
+            '}';
+
+    }
 }
--- src/main/java/it/universita/model/Materia.java
+++ src/main/java/it/universita/model/Materia.java
@@ -1,4 +1,79 @@
 package it.universita.model;
 
 public class Materia {
+    private long id;
+    private long id_corso;
+    private String nome;
+    private int cfu;
+    private int anno;
+    private int semestre;
+
+public Materia(){}
+
+    public Materia (long id, long id_corso, String nome, int cfu, int anno) {
+        this.id = id;
+        this.id_corso = id_corso;
+        this.nome = nome;
+        this.cfu = cfu;
+        this.anno = anno;
+        this.semestre = 0;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId_corso() {
+     return id_corso;
+    }
+
+    public void setId_corso(long id_corso) {
+        this.id_corso = id_corso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+     this.nome = nome;
+    }
+
+    public int getCfu() {
+     return cfu;
+    }
+
+    public void setCfu(int cfu) {
+        this.cfu = cfu;
+    }
+
+    public int getAnno() {
+     return anno;
+    }
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+    @Override
+    public String toString() {
+        return "Materia{" +
+                "id=" + id +
+                ", id_corso=" + id_corso +
+                ", nome='" + nome + '\'' +
+                ", cfu=" + cfu +
+                ", anno=" + anno +
+                ", semestre=" + semestre +
+                '}';
+
+    }
 }
+
